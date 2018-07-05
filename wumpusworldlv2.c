fe95b0f307636c0a8d40d041c62c3ffcc530c4cb
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-07-05 22:25:58 +0530

    Update wumpusworldlv2.c

--- wumpusworldlv2.c
+++ wumpusworldlv2.c
@@ -7,7 +7,7 @@ int flag2[50];
 char start;
 char question[20];
 
-void reveal()
+void reveal()  // function to reveal each square as it is visited
 {
 
         if (start==25&&flag2[start]==1)
@@ -142,7 +142,7 @@ void reveal()
 }
 
 
-representation()
+representation() // function to recursively print all the squares/map
         {
             printf("+---+---+---+---+ \n");
                 printf("|%c%c%c|%c%c%c|%c%c%c|%c%c%c|\n",p[1],question[1],p[2],p[3],question[2],p[4],p[5],question[3],p[6],p[7],question[4],p[8]);
@@ -163,201 +163,158 @@ representation()
 
 void main()
         {
-                int flag =0,flag1,point=0;
-                char mov;
-                for(int i=0;i<50;i++)
-                {
-                    question[i]='?';
-                    flag2[i]=1;
-                    p[i]=' ';
-                }
-                start=37;
+            int flag =0,flag1,point=0;
+            char mov;
+            for(int i=0;i<50;i++)
+            {
+                question[i]='?';
+                flag2[i]=1;
+                p[i]=' ';
+            }
+            start=37;
             p[start]='^';
             reveal();
-                representation();
-                
-                while(flag!=1)
-                {
-                        printf("enter l for turning left, r for turning right,f for moving forward in the facing direction,g for grabbing and s for shooting \n");
-                        scanf("%c",&mov);
-                        //printf("%c",mov);
+            representation();   
+            while(flag!=1)
+            {
+                    printf("enter l for turning left, r for turning right,f for moving forward in the facing direction,g for grabbing and s for shooting \n");
+                    scanf("%c",&mov);
                     if(mov=='l')
-                        {
-                                if(p[start]=='^')
-                                 p[start]='<';
+                    {
+                        if(p[start]=='^')
+                          p[start]='<';
+                        else if(p[start]=='<')
+                         p[start]='v';
+                        else if(p[start]=='v')
+                         p[start]='>';
+                        else
+                         p[start]='^';      
+                        representation();
+                    }
+
+                   if(mov=='r')
+                    {
+                        if(p[start]=='^')
+                         p[start]='>';
+                        else if(p[start]=='<')
+                         p[start]='^';
+                        else if(p[start]=='v')
+                         p[start]='<';
+                        else
+                         p[start]='v';      
+                            representation();
+                    }
+
+                   if(mov=='f')
+                    {
+                            char temp=p[start];
+                            int temp1=start;
+                            if(p[start]=='^')
+                            {
+                              if(start==25||start==26||start==27||start==28)
+                                    printf("invalid move try again \n"); 
+                              else
+                               start-=4;
+                              reveal();
+                            }
                             else if(p[start]=='<')
-                             p[start]='v';
+                            {
+                              if(start==25||start==29||start==33||start==37)
+                                    printf("invalid move try again \n");   
+                              else
+                               start-=1;
+                              reveal();
+
+                           }
                             else if(p[start]=='v')
-                             p[start]='>';
+                             {
+                              if(start==37||start==38||start==39||start==40)
+                                    printf("invalid move try again \n");   
+                              else
+                               start+=4;
+                              reveal();
+                             }
                             else
-                             p[start]='^';      
-                                representation();
-                        }
-
-                        if(mov=='r')
-                        {
-                                if(p[start]=='^')
-                                 p[start]='>';
-                            else if(p[start]=='<')
-                             p[start]='^';
-                            else if(p[start]=='v')
-                             p[start]='<';
-                            else
-                             p[start]='v';      
-                                representation();
-                        }
-
-                        if(mov=='f')
-                        {
-                                char temp=p[start];
-                                int temp1=start;
-                                if(p[start]=='^')
-                                {
-                                  if(start==25)
-                                        printf("invalid move try again \n");
-                                  else if(start==26)
-                                        printf("invalid move try again \n");
-                                  else if(start==27)
-                                        printf("invalid move try again \n");
-                                  else if(start==28)
-                                        printf("invalid move try again \n");    
-                                  else
-                                   start-=4;
-                                   reveal();
-                                }
-                                else if(p[start]=='<')
-                                {
-                                  if(start==25)
-                                        printf("invalid move try again \n");
-                                  else if(start==29)
-                                        printf("invalid move try again \n");
-                                  else if(start==33)
-                                        printf("invalid move try again \n");
-                                  else if(start==37)
-                                        printf("invalid move try again \n");    
-                                  else
-                                   start-=1;
-                                   reveal();
-
-                                }
-                                else if(p[start]=='v')
-                                 {
-                                  if(start==37)
-                                        printf("invalid move try again \n");
-                                  else if(start==38)
-                                        printf("invalid move try again \n");
-                                  else if(start==39)
-                                        printf("invalid move try again \n");
-                                  else if(start==40)
-                                        printf("invalid move try again \n");    
-                                  else
-                                        start+=4;
-                                    reveal();
-                                 }
-                                else
-                                {
-                                  if(start==28)
-                                        printf("invalid move try again \n");
-                                  else if(start==32)
-                                        printf("invalid move try again \n");
-                                  else if(start==36)
-                                        printf("invalid move try again \n");
-                                  else if(start==40)
-                                        printf("invalid move try again \n");    
-                                  else
-                                 start+=1;
-                                 reveal();
-                                }
-                                p[temp1]=' ';
+                            {
+                              if(start==28||start==32||start==36||start==40)
+                                    printf("invalid move try again \n");   
+                              else
+                               start+=1;
+                              reveal();
+                            }
+                            p[temp1]=' ';
                             p[start]=temp;
                             point-=1;
-                                representation();
-                        }
+                            representation();
+                    }
 
-                        if(mov=='s')
-                        {
-                                if(p[start]=='^'&&start==33)
-                           {
-                                                p[9]=' ';
-                                                printf("you have shot the wumpus!! \n");
-                                                shot=1;
-                                                point-=10;
-                                }
-                                else if(p[start]=='<'&&start==30)
-                                {
-                                                p[9]=' ';
-                                                printf("you have shot the wumpus!! \n");
-                                                shot=1;
-                                                point-=10;
-                                }
-                                else if(p[start]=='v'&&start==25)
-                                {
-                                        p[9]=' ';
-                                        printf("you have shot the wumpus!! \n");
-                                        shot=1;
-                                        point-=10;
-                                }
-                                else if(p[start]=='>'&&p[start+1]=='W')
-                                {
-                                                p[9]=' ';
-                                                printf("you have shot the wumpus!! \n");
-                                                shot=1;
-                                                point-=10;
-                                }
-                                else
-                                {
-                                        printf("arrow shot , no wumpus present !! \n");
-                                        point-=10;
-
-                                }
-
-                        }
-
-
-                        if(mov=='g')  // must write present on gold square
-                        {
-                                if(start==30)
-                                {
-                                point+=1000;
-                                printf("you have grabbed the gold, go back to start \n");
-                                printf("or do you want to exit? press 1 for yes , 0 for no \n");
-                                scanf("%d",&flag1);
-                                if(flag1==1)
-                                        flag=1;
+                   if(mov=='s')
+                    {
+                            if(p[start]=='^'&&start==33)
+                            {
+                              p[9]=' ';
+                              printf("you have shot the wumpus!! \n");
+                              shot=1;
+                              point-=10;
+                            }
+                            else if(p[start]=='<'&&start==30)
+                            {
+                              p[9]=' ';
+                              printf("you have shot the wumpus!! \n");
+                              shot=1;
+                              point-=10;
+                            }
+                            else if(p[start]=='v'&&start==25)
+                            {
+                              p[9]=' ';
+                              printf("you have shot the wumpus!! \n");
+                              shot=1;
+                              point-=10;
+                            }
+                            else if(p[start]=='>'&&p[start+1]=='W')
+                            {
+                              p[9]=' ';
+                              printf("you have shot the wumpus!! \n");
+                              shot=1;
+                              point-=10;
                             }
                             else
                             {
-                                printf("nothing to grab here , try in another square \n");
+                              printf("arrow shot , no wumpus present !! \n");
+                              point-=10;
                             }
-                        }
-                        if(start==29&&p[9]=='W')
-                        {
-                                printf("wumpus detected, you are dead!! \n ");
-                                point-=1000;
-                                flag=1;
-                        }
-                        if(start==28) // 31,39
-                        {
-                                printf("Pit detected, you are dead!! \n ");
-                                point-=1000;
-                                flag=1;
-                        }
-                        if(start==31) // 31,39
-                        {
-                                printf("Pit detected, you are dead!! \n ");
-                                point-=1000;
-                                flag=1;
-                        }
-                        if(start==39) // 31,39
-                        {
-                                printf("Pit detected, you are dead!! \n ");
-                                point-=1000;
-                                flag=1;
-                        }
-                }
-                printf(" your score is %d \n",point);
-    }
+                   }
 
+                   if(mov=='g')  // function to grab gold
+                    {
+                            if(start==30)
+                            {
+                            point+=1000;
+                            printf("you have grabbed the gold congratulations,now go back to start \n");
+                            printf("or do you want to exit? press 1 for yes , 0 for no \n");
+                            scanf("%d",&flag1);
+                            if(flag1==1)
+                                    flag=1;
+                        }
+                        else
+                        {
+                            printf("nothing to grab here , try in another square \n");
+                        }
+                    }
+                    if(start==29&&p[9]=='W')  // interaction with Wumpus
+                    {
+                            printf("wumpus detected, you are dead!! \n ");
+                            point-=1000;
+                            flag=1;
+                    }
+                    if(start==31||start==39||start==28) 
+                    {
+                            printf("Pit detected, you are dead!! \n ");
+                            point-=1000;
+                            flag=1;
+                    }
+            }
+            printf(" game over,your score is %d \n",point);
+}
 
 //left,right,forward,grab,shoot
-
