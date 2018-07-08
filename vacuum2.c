9e628fb1d856efa796f653dc3410872282ea8d2a
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-07-08 21:49:48 +0530

    Create vacuum2.c

---
+++ vacuum2.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int j=1,area[20];
+int right()
+{
+ printf("block %d empty \t \t \t \t \t \t right \n ",j);
+ j=j+1;
+}
+
+int left()
+{
+ j=j-1;
+}
+
+int suck(int n)
+{
+ if(j<n)
+ {
+  printf("block %d dirty \t \t \t \t \t \t  suck \n ",j);
+  printf("block %d clean \t \t \t \t \t \t  right \n ",j );
+ }
+ if(j==n)
+ {
+  printf("block %d dirty \t \t \t \t \t \t  suck \n ",j);
+  printf("block %d clean \t \t \t \t \t \t  left \n ",j );
+ }
+ area[j++]=0;
+ //printf("block %d has been cleaned and now moves right to block %d
+to check for dirtyness \n",j-1,j);
+
+}
+
+void main()
+{
+ int area[20],i,n;
+ printf("enter the number of blocks");
+ scanf("%d",&n);
+ printf("enter the pattern of dirt to be cleaned , enter 1 for dirt, 0
+for clean \n");
+ for(i=1;i<=n;i++)
+ {
+  scanf("%d",&area[i]);
+ }
+ while(j<=n)
+ {
+  if(area[j]==1)
+   suck(n);
+  else
+   right();
+ }
+}
