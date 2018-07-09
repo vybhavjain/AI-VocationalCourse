6555047f4e9a8933fa8b70c9647f93f5cdb617d7
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-07-09 16:18:47 +0530

    Update vacuum1.c

--- vacuum1.c
+++ vacuum1.c
@@ -1,17 +1,17 @@
 #include<stdio.h>
 int j=1,area[20];
-int right()
+int right() // used to move the block pointer in the right direction
 {
  printf("block %d is perceived empty, so now we move right \n ",j);
  j=j+1;
 }
 
-int left()
+int left()  // used to move the block pointer in the left direction
 {
  j=j-1;
 }
 
-int suck()
+int suck() // clears the block of any dirt
 {
  printf("block %d has been detected dirty \n ",j);
  area[j++]=0;
