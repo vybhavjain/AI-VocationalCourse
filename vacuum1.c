37a1ecef5294dbaa17ee823fcf784638c7773157
Vybhav Jain <35528017+vybhavjain@users.noreply.github.com>
2018-07-17 13:28:41 +0530

    Update vacuum1.c

--- vacuum1.c
+++ vacuum1.c
@@ -5,25 +5,27 @@ int right() // used to move the block pointer in the right direction
  printf("block %d is perceived empty, so now we move right \n ",j);
  j=j+1;
 }
-
+ 
+/*  // function not used here as we assume the pointer to be at the start point and not in the middle.
 int left()  // used to move the block pointer in the left direction
 {
  j=j-1;
 }
+*/
 
 int suck() // clears the block of any dirt
 {
  printf("block %d has been detected dirty \n ",j);
  area[j++]=0;
- printf("block %d has been cleaned and now moves right to block %d to check for dirtyness \n",j-1,j);
+ printf("block %d has been cleaned and now moves right to block %d to perceive for dirtyness \n",j-1,j);
 }
 
 void main()
 {
- int area[20],i,n;
+ int i,n;
  printf("enter the number of blocks");
  scanf("%d",&n);
- printf("enter the pattern of dirt to be cleaned , enter 1 for dirt, 0 for clean \n");
+ printf("enter the pattern of dirt to be cleaned,enter 1 for dirt, 0 for clean \n");
  for(i=1;i<=n;i++)
   scanf("%d",&area[i]);
  while(j<=n)
